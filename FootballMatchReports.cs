629d555b7746f4a41711445f0dd80ab65e7ab7a8
Dmitriy Sukharev <dmitriy.sukharev@fullhpltd.com>
2024-06-04 20:27:10 +0300

    [feat] implement description output

--- FootballMatchReports.cs
+++ FootballMatchReports.cs
@@ -1,11 +1,24 @@
 using System;
+using System.Diagnostics;
 
 public static class PlayAnalyzer
 {
-    public static string AnalyzeOnField(int shirtNum)
-    {
-        throw new NotImplementedException($"Please implement the (static) PlayAnalyzer.AnalyzeOnField() method");
-    }
+    public static string AnalyzeOnField(int shirtNum) => 
+        shirtNum switch 
+        {
+            1 => "goalie",
+            2 => "left back",
+            3 => "center back",
+            4 => "center back",
+            5 => "right back",
+            6 => "midfielder",
+            7 => "midfielder",
+            8 => "midfielder",
+            9 => "left wing",
+            10 => "striker",
+            11 => "right wing",
+            _ => throw new ArgumentOutOfRangeException()
+        };
 
     public static string AnalyzeOffField(object report)
     {
